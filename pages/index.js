848e5324567647a17f58db6993b099e90857e8c5
Long Pham <long29031997@gmail.com>
2021-11-29 20:54:39 +0700

    Ahihi

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 12!</a>
+          Welcome to <a href="https://nextjs.org">Next.js 122!</a>
         </h1>
 
         <p className={styles.description}>
